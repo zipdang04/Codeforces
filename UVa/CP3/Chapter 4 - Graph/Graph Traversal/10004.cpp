93c0bb1a942dac68e1f48d284745278e876ae6a2
Đặng Xuân Minh Hiếu <zipdang2004@gmail.com>
2020-02-22 22:18:27 +0700

    AC
    AC

--- UVa/CP3/Chapter 4 - Graph/Graph Traversal/10004.cpp
+++ UVa/CP3/Chapter 4 - Graph/Graph Traversal/10004.cpp
@@ -1,42 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define MAX 201
 
-int n, m;
 vector<int> graph[MAX];
-int color[MAX] = {};
-
-void refresh(){
-	for (int i = 1; i <= MAX; i++){
-		graph[i].clear();
-	}
-	memset(color, 0, sizeof(color));
-}
+int n, m;
+int state[MAX];
 
 bool test(int node, int color){
-	int newColor = 3 - color;
+	int other = 3 - color;
 	for (int child: graph[node]){
-		if (color[child]){
-//			if (color[child] == color) return false;
-//			continue;
+		if (state[child]){
+			if (state[child] == color) return false;
+			continue;
 		}
-//		color[child] = newColor;
-//		if (!test(child, newColor)) return false;
+		state[child] = other; 
+		if (!test(child, other)) return false;
 	}
 	return true;
+}
+
+void refresh(){
+	for (int i = 1; i < MAX; i++) graph[i].clear();
+	memset(state, 0, sizeof(state));
 }
 
 main(){
 	while (true){
 		cin >> n; if (n == 0) break; cin >> m;
-		for (int i = 0; i < m; i++){
+		for (int i = 1; i <= m; i++){
 			int u, v; cin >> u >> v; u++, v++;
 			graph[u].push_back(v);
 			graph[v].push_back(u);
 		}
-		color[1] = 1;
+		state[1] = 1;
 		if (!test(1, 1)) cout << "NOT ";
 		cout << "BICOLORABLE.\n";
+		refresh();
 	}
 }

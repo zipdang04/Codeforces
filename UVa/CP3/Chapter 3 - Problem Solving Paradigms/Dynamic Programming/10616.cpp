380e82cb4185e6be90badfad68630e5711a8e2cb
Đặng Xuân Minh Hiếu <zipdang2004@gmail.com>
2020-02-22 15:17:49 +0000

    AC

--- UVa/CP3/Chapter 3 - Problem Solving Paradigms/Dynamic Programming/10616.cpp
+++ UVa/CP3/Chapter 3 - Problem Solving Paradigms/Dynamic Programming/10616.cpp
@@ -1,69 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, q;
+typedef long long ll;
 
-string add(string a, string b)
-{
-    string res="";
-    while(a.length() < b.length()) a="0"+a;
-    while(b.length() < a.length()) b="0"+b;
-    int carry=0;
-    for(int i=a.length()-1;i>=0;i--)
-    {
-        int tmp=a[i]-48 + b[i]-48 + carry;
-        carry=tmp/10;
-        tmp=tmp%10;
-        res=(char)(tmp+48)+res;
-    }
-    if(carry>0) res="1"+res;
-    return res;
+ll n, q;
+ll ans[201][11][21], arr[201];
+
+void refresh(){
+	memset(ans, 0, sizeof(ans));
 }
 
-string convert(int num){
-    string ans = "";
-    while (num > 0){
-        ans = char(num % 10 + '0') + ans, num /= 10;
-    }
-    return ans;
-}
+main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	for (ll iii = 1; ; iii++){
+		cin >> n >> q; if (n == 0 && q == 0) break;
+		cout << "SET " << iii << ":\n";
 
-int main(int argc, char const *argv[])
-{
-    ios_base::sync_with_stdio(0); cin.tie(NULL);
-    for (int iiiii = 1; ; iiiii++)
-    {
-        cin >> n >> q;
-        if (n == 0 && q == 0){return 0;}
-        cout << "SET " << iiiii << ":\n";
-        int arr[n + 1] = {};
-        for (int i = 1; i <= n; i++) cin >> arr[i];
-        for (int iii = 0; iii < q; iii++)
-        {
-            cout << "QUERY " << iii + 1 << ": ";
-            int d, m; cin >> d >> m;
-            string ans[11][21] = {};
-            /**/
-            for (int j = 1; j <= m; j++)
-                for (int r = 0; r < d; r++)
-                    ans[j][r] = "0";
-            /**/
-            for (int i = 1; i <= n; i++)
-            {
-                string now[11][21] = {};
-                int currentRemain = arr[i] % d;
-                now[1][currentRemain] = "1";
-                for (int j = 2; j <= min(i, m); j++)
-                    for (int r = 0; r < d; r++)
-                        now[j][r] = ans[j - 1][(r + d - currentRemain) % d];
-                for (int j = 1; j <= m; j++)
-                    for (int r = 0; r < d; r++)
-                        ans[j][r] = add(ans[j][r], now[j][r]);
-            }
-            cout << ans[m][0] << '\n';
-        }
-        
-    }
-    
-    return 0;
+		for (ll i = 1; i <= n; i++) cin >> arr[i];
+		for (ll ii = 1; ii <= q; ii++){
+			cout << "QUERY " << ii << ": ";
+			ll d, m; cin >> d >> m;
+			ans[0][0][0] = 1;
+			ll finalAns = 0;
+			for (ll i = 1; i <= n; i++){
+				ll r = arr[i] % d;
+				
+				for (ll j = 0; j <= m; j++)
+					for (ll k = 0; k < d; k++) ans[i][j][k] = ans[i - 1][j][k];
+
+				for (ll j = 1; j <= min(i, m); j++){
+					for (ll k = 0; k < d; k++){
+						ll r2 = (k + r) % d; if (r2 < 0) r2 += d;
+						ans[i][j][r2] += ans[i - 1][j - 1][k];
+					}
+				}
+			}
+			cout << ans[n][m][0] << '\n';
+			refresh();
+		}
+	}
 }
